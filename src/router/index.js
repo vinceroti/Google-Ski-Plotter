ff4b385d63a84ebf2d9e055ac97356ab0d827854
Vince Roti <vince.roti@gmail.com>
2018-03-03 15:49:22 -0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import GoogleMaps from '@/components/GoogleMaps'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
