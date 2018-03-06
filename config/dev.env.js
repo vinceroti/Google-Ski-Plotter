7c1cd3bcc8085737d2c19f237ac59f44522a32b5
Vince Roti <vince.roti@gmail.com>
2018-03-06 07:48:04 -0800

    add script and reduce json

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,9 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+require('dotenv').config()
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  MAP_KEY: JSON.stringify(process.env.MAPS),
 })
