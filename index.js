c8757bb6981c70fd8edd00a235f713d1d87096e2
Vince Roti <vince.roti@gmail.com>
2018-03-19 09:55:05 -0700

    more logic checking and add pm2 module

--- index.js
+++ index.js
@@ -3,24 +3,36 @@ const app = express();
 const JSONStream = require('JSONStream')
 const fs = require("fs");
 
+const mountains = {
+  'crystal': [(46.9282  * 1e7) , (-121.5045  * 1e7)],
+  'snoqual': [(47.43916491 * 1e7), (-121.424331636 * 1e7)]
+}
+
 app.get('/api', (req, res) => {
 
-  var array = [];
+  let array = [];
 
-  var stream = fs.createReadStream('./static/location.json'),
+  let stream = fs.createReadStream('./static/location-full.json'),
       parser = JSONStream.parse('*.*');
 
   res.set('Access-Control-Allow-Origin', '*')
 
   stream.pipe(parser);
 
-  parser.on('data', function (obj) {
-    if (obj.latitudeE7 > (47.3823 * 1e7) && obj.latitudeE7 < (47.4023 * 1e7)) {
-      array.push(obj)
+  parser.on('data',  (obj) => {
+    for (let mountain in mountains) {
+      if (
+        (obj.latitudeE7 > (mountains[mountain][0] - 300000) && obj.latitudeE7 < (mountains[mountain][0]  + 300000)) &&
+        ((obj.longitudeE7 > (mountains[mountain][1] - 300000) && obj.longitudeE7 < (mountains[mountain][1]  + 300000)))
+        ) {
+        array.push(obj);
+        return;
+      }
     }
+
   });
 
-  parser.on('end', function () {
+  parser.on('end', () => {
     res.json(array);
   });
 
