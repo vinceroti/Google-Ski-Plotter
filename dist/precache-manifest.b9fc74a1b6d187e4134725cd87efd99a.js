e5a6d8f9d3668ce28e8a90eb2d75014ebd274974
Vince Roti <vince.roti@gmail.com>
2018-11-09 11:35:54 -0800

    new build

---
+++ dist/precache-manifest.b9fc74a1b6d187e4134725cd87efd99a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0679d6d4bb84d1352e81",
+    "url": "/css/app.7681fe1d.css"
+  },
+  {
+    "revision": "0679d6d4bb84d1352e81",
+    "url": "/js/app.251f2c06.js"
+  },
+  {
+    "revision": "b71b5895771c5ba2aca2",
+    "url": "/css/chunk-vendors.3c877f64.css"
+  },
+  {
+    "revision": "b71b5895771c5ba2aca2",
+    "url": "/js/chunk-vendors.e8ced464.js"
+  },
+  {
+    "revision": "801e4b3d729306c54a0d5a3889801753",
+    "url": "/img/marker.801e4b3d.png"
+  },
+  {
+    "revision": "6866a4eb3831afc3a46e1438598e8157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
