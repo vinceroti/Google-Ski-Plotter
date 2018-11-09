a68d94cf2e45fad8200f8ece4eaab92ce6b4d3ad
Vince Roti <vince.roti@gmail.com>
2018-11-08 16:45:50 -0800

    add git ignore file

---
+++ dist/precache-manifest.f1ea03e0b9b7b5c26a412bae3673d893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2748afc8800d43bdf837",
+    "url": "/css/app.870910b2.css"
+  },
+  {
+    "revision": "2748afc8800d43bdf837",
+    "url": "/js/app.55324ef3.js"
+  },
+  {
+    "revision": "090d8d4ad3f65538d524",
+    "url": "/css/chunk-vendors.3c877f64.css"
+  },
+  {
+    "revision": "090d8d4ad3f65538d524",
+    "url": "/js/chunk-vendors.a5092d6f.js"
+  },
+  {
+    "revision": "801e4b3d729306c54a0d5a3889801753",
+    "url": "/img/marker.801e4b3d.png"
+  },
+  {
+    "revision": "782e048ffdf431a137f0d11c8af22cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
